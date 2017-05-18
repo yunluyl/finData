c5b9334d6409abe8f97d09bece2b2089fb7b24ec
Yun Lu <luyun198993@gmail.com>
2017-05-18 16:15:35 -0500

    fixes

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,15 @@
 var app = require('../app');
-var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 3000;
-
+/*
 var options =
 {
 	key : fs.readFileSync('./certs/server.key'),
 	cert : fs.readFileSync('./certs/server.cert')
 };
-var server = https.createServer(options, app);
+*/
+var server = http.createServer(app);
 server.on('error', onError);
 server.listen(port, function()
 {
